3bc69ecff2a3dde4d6ad879e3fe46be5765e3493
BRENO OLIVEIRA ASSIS REIS <25133656@cfp127aluno.local>
2025-04-02 11:23:06 -0300

    Criando uma Tabuada

---
+++ src/br/dev/breno/Tabuada/model/Tabuada.java
@@ -0,0 +1,59 @@
+package br.dev.breno.Tabuada.model;
+
+public class Tabuada {
+	private double multiplicando;
+	private double minimoMultiplicador;
+	private double maximoMultiplicador;
+	// Atributos privados(não aparece para outras classes)
+
+	public double getMultiplicando() {
+		return multiplicando;
+		// get(retira algo da classe)
+	}
+
+	public void setMultiplicando(double multiplicando) {
+		this.multiplicando = multiplicando;
+		// this é quando estamos se referiando a propria classe(quando estamos dentro
+		// dela)
+		// recebe multiplicando(set é de inserir algo)
+	}
+
+	public double getMinimoMultiplicador() {
+		return minimoMultiplicador;
+	}
+
+	public void setMinimoMultiplicador(double minimoMultiplicador) {
+		this.minimoMultiplicador = minimoMultiplicador;
+	}
+
+	public double getMaximoMultiplicador() {
+		return maximoMultiplicador;
+	}
+
+	public void setMaximoMultiplicador(double maximoMultiplicando) {
+		this.maximoMultiplicador = maximoMultiplicando;
+	}
+
+	public void mostrarTabuada() {
+		System.out.println("Tabuada do" + multiplicando);
+
+		if (minimoMultiplicador > maximoMultiplicador) {
+			double troca = minimoMultiplicador;
+			minimoMultiplicador = maximoMultiplicador;
+			maximoMultiplicador = troca;
+		}
+
+		while (minimoMultiplicador <= maximoMultiplicador) {
+			double produto = multiplicando * minimoMultiplicador;
+			System.out.println("multiplicando" + "X" + minimoMultiplicador + " =" + produto);
+			minimoMultiplicador++;
+
+		}
+		System.out.println();
+		System.out.println("Fim daTabuada do: " + multiplicando);
+		System.out.println("---------------------------------");
+		System.out.println();
+
+	}
+
+}
